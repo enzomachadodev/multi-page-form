65b32bb2cf3e83eb5e892524765f2b756d5ecafb
Enzo Machado <enzo653fm@gmail.com>
2024-08-30 00:48:01 -0300

    feat: add StepThreeForm component

---
+++ src/app/add/step-three/page.tsx
@@ -0,0 +1,12 @@
+import { StepThreeForm } from "./step-three-form";
+
+const StepThreePage = () => {
+	return (
+		<div>
+			<StepThreeForm />
+		</div>
+	);
+};
+
+export default StepThreePage;
+
---
+++ src/app/add/step-three/step-three-form.tsx
@@ -0,0 +1,25 @@
+import { FormInput } from "@/components/form-input";
+import { SubmitButton } from "@/components/ui/submit-button";
+
+export const StepThreeForm = () => {
+	return (
+		<form className="flex flex-1 flex-col">
+			<div className="flex w-full flex-col lg:max-w-[700px]">
+				<FormInput
+					label="Contact Name"
+					id="contactName"
+					type="text"
+					required
+				/>
+				<FormInput
+					label="Contact Email"
+					id="contactEmail"
+					type="email"
+					required
+				/>
+				<SubmitButton>Submit</SubmitButton>
+			</div>
+		</form>
+	);
+};
+
